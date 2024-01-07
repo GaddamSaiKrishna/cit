8726f31197bd7fd495c8c3328ca95c7f854b0dbd
Avinash <avinash31803@gmail.com>
2024-01-07 23:15:03 +0530

    updated images

--- CIT/CIT/urls.py
+++ CIT/CIT/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
@@ -29,5 +29,5 @@ urlpatterns = [
 
 urlpatterns += staticfiles_urlpatterns()
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
