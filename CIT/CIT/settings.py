94949fe2610c76c6a8751b2163262f50b3345553
Avinash <avinash31803@gmail.com>
2024-01-10 19:58:43 +0530

    auth check

--- CIT/CIT/settings.py
+++ CIT/CIT/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'CIT.urls'
@@ -91,7 +92,7 @@ DATABASES = {
 }
 
 DATABASE_URL = config('DATABASE_URL')
-DATABASES["default"] = dj_database_url.parse(DATABASE_URL)
+# DATABASES["default"] = dj_database_url.parse(DATABASE_URL)
 
 
 # Password validation
